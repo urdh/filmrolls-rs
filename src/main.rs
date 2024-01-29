40eca1a10f4567fc658f84664c950d5c390c3e57
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2024-01-29 17:14:46 +0100

    Implement list-rolls & list-frames in the CLI

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+#![doc = include_str!("../README.md")]
+use std::process::ExitCode;
+
+use ::clap::Parser;
+use color_eyre::eyre::Result;
+
+mod clap;
+mod cmds;
 mod rolls;
 mod types;
 
-fn main() {}
+fn main() -> Result<ExitCode> {
+    let args = clap::Cli::parse();
+    args.init_colors()?;
+    args.init_logging();
+    args.run_command()
+}
